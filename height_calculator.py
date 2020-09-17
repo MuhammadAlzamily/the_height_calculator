7c63055292c489992fe2909ec66ba9bf05aa3582
MuhammadAlzamily <xxhopkinsxx1998@gmail.com>
2020-09-18 02:15:52 +0300

    redid the layout

--- height_calculator.py
+++ height_calculator.py
@@ -1,17 +1,21 @@
 from tkinter import *
-from tkinter.messagebox import showinfo,showerror
+from tkinter.messagebox import showinfo, showerror
+
+
 def showheight():
-	if height.get():
-		showinfo(title="Results",message=f"Your height is {height.get()}")
-	else:
-		showerror(title="Error",message="Text field can't be empty")
+    if height.get():
+        showinfo(title="Results", message=f"Your height is {height.get()}")
+    else:
+        showerror(title="Error", message="Text field can't be empty")
+
+
 root = Tk()
 root.title("Height Calculator")
 root.geometry("300x150")
-lbl = Label(root,text="Enter your height")
+lbl = Label(root, text="Enter your height")
+lbl.pack(pady=15)
 height = Entry(root)
-btn = Button(root,text="Click me :)",command=showheight)
-lbl.grid(row=1,column=1)
-height.grid(row=1,column=2)
-btn.grid(row=3,column=2)
+height.pack(pady=15)
+btn = Button(root, text="Click me :)", command=showheight)
+btn.pack(pady=15)
 root.mainloop()
