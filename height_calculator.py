54d82285853c4c04c69077c84b1ae3719258f696
MuhammadAlzamily <xxhopkinsxx1998@gmail.com>
2020-09-18 22:35:35 +0300

    fixed text field

--- height_calculator.py
+++ height_calculator.py
@@ -7,16 +7,15 @@ def showheight():
     if height.get():
         showinfo(title="Results", message=f"Your height is {height.get()}")
     else:
-        showerror(title="Error", message="Text field can't be empty")
+        showerror(title="Empty Field", message="Text field can't be empty")
 
 
 root = Tk()
 root.title("Height Calculator")
 root.geometry("500x400")
-root.configure({"bg": "pink"})
 lbl = Label(root, text="Enter your height", font=("bold", 18))
 lbl.pack(pady=15)
-height = ttk.Entry(root)
+height = ttk.Entry(root, width=30)
 height.pack(pady=15, ipadx=25, ipady=5)
 btn = ttk.Button(root, text="Calculate", command=showheight)
 btn.pack(pady=15)
