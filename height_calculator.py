9fc9aa4d8f644845d0888bc866b394fe6d24c2e0
MuhammadAlzamily <xxhopkinsxx1998@gmail.com>
2020-09-18 20:08:19 +0300

    pink bg

--- height_calculator.py
+++ height_calculator.py
@@ -13,7 +13,7 @@ def showheight():
 root = Tk()
 root.title("Height Calculator")
 root.geometry("500x400")
-root.configure({"bg": "aqua"})
+root.configure({"bg": "pink"})
 lbl = Label(root, text="Enter your height")
 lbl.pack(pady=15)
 height = ttk.Entry(root)
